0adfc7a424d08794c2d2dfa0973d4ac4abaff79d
MicRedd1985 <micredd1985@gmail.com>
2022-12-15 17:55:12 +0200

    Revert "complete task"
    This reverts commit d9ee4491438b155175177e3df7ab6a25c8e55c5a.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,16 @@
-import { Profile } from "./profile/Profile"; 
-import { Statistics } from "./statistics/statistics";
-import { FriendList } from "./FriendList/FriendList";
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
-
-import user from '../files from task/user';
-import data from '../files from task/data';
-import friends from '../files from task/friends';
-import transactions from '../files from task/transactions';
-
 export const App = () => {
   return (
     <div
       style={{
-        height: 'auto',
+        height: '100vh',
         display: 'flex',
-        flexDirection: "column",
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 20,
-        color: '#010101',
+        fontSize: 40,
+        color: '#010101'
       }}
     >
-      <Profile avatar={user.avatar}
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-      
-      </div>
+      React homework template
+    </div>
   );
 };
